3b7c6c8d476e7422a4ccfbfc3461fe37abe97a3f
Ruslan Sadkevich <andreiloverus@gmail.com>
2026-04-15 22:12:45 +0300

    part1

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,25 +1,20 @@
 # gameparts/parts.py
 
-class Board:
-    """Класс, который описывает игровое поле."""
-
-    field_size = 3
-
-    def __init__(self):
-        self.board = [
-            [' ' for _ in range(self.field_size)] for _ in range(self.field_size)
-        ]
-
-    def make_move(self, row, col, player):
-        self.board[row][col] = player
+...
 
     def display(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 5)
+        ...
 
-    def __str__(self):
-        return (
-            'Объект игрового поля размером '
-            f'{self.field_size}x{self.field_size}'
-        )
+    def is_board_full(self):
+        # Цикл проходится по всем столбцам игрового поля.
+        for i in range(self.field_size):
+            # А потом по всем строчкам.
+            for j in range(self.field_size):
+                # Если находит свободную ячейку...
+                if self.board[i][j] == ' ':
+                    # ...игра продолжается.
+                    return False
+        # Иначе - ничья!
+        return True
+
+...
