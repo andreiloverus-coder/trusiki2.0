06745474381fe0560513a52ce6261e25ea7c1809
Ruslan Sadkevich <andreiloverus@gmail.com>
2026-04-15 22:16:17 +0300

    act1

--- tic tac toe/file_actions.py
+++ tic tac toe/file_actions.py
@@ -1,8 +1,10 @@
 # file_actions.py
 
-# Открыть на запись файл example.txt
-file = open('example.txt', 'w')
-# Записать в файл строку.
-file.write('Зевну, укроюсь с головою,\nбудильник заведу на март.\n')
+# Открыть файл example.txt на чтение (аргумент 'r').
+file = open('example.txt', 'r')
+# Прочитать первые 12 символов из файла и сохранить их в переменную content.
+content = file.read(12)
+# Вывести на печать содержимое переменной content.
+print(content)
 # Закрыть файл.
 file.close()
