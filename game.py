de80da0cbef854e487d3bfdb9a8dd0cfd7f57a16
Ruslan Sadkevich <andreiloverus@gmail.com>
2026-04-15 21:55:43 +0300

    eee

--- game.py
+++ game.py
@@ -2,8 +2,14 @@
 
 from gameparts import Board
 
-game = Board()
-game.display()
-game.make_move(1, 1, 'X')
-print('Ход сделан!')
-game.display()
+# Вот новая функция.
+def main():
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    game.display()
+
+# А вот вызов этой функции.
+if __name__ == '__main__':
+    main()
