0ed68ad9635784d56f492bbb57d1b14ccf09e890
Ruslan Sadkevich <andreiloverus@gmail.com>
2026-04-15 22:01:11 +0300

    doccod

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,6 +1,8 @@
-class Board:
+# gameparts/parts.py
 
-    # Новый атрибут.
+class Board:
+    """Класс, который описывает игровое поле."""
+
     field_size = 3
 
     def __init__(self):
@@ -16,9 +18,8 @@ class Board:
             print('|'.join(row))
             print('-' * 5)
 
-    # Переопределяем метод __str__.
     def __str__(self):
         return (
             'Объект игрового поля размером '
             f'{self.field_size}x{self.field_size}'
-        ) 
+        )
